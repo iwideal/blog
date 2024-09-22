0b60120054daac0019cead91dfe1e0d5e2691c96
iwideal <iwideal@outlook.com>
2024-09-22 10:54:33 +0800

    111

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,11 @@
     <header class="bg-white">
         <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a href="#" class="-m-1.5 p-1.5">
+                <button @click="$router.push('/')" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
                     <img class="h-8 w-auto"
                         src="https://tailwindui.starxg.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-                </a>
+                </button>
             </div>
             <div class="flex lg:hidden">
                 <button type="button"
@@ -17,8 +17,8 @@
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-                <a v-for="item in navigation" :key="item.name" :href="item.href"
-                    class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
+                <button v-for="item in navigation" :key="item.name" @click="$router.push(item.href)"
+                    class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</button>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
@@ -69,10 +69,10 @@ import { Dialog, DialogPanel } from '@headlessui/vue'
 import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
 
 const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+    { name: '首页', href: '/' },
+    { name: '博客', href: '/blog' },
+    { name: '关于我', href: '/about' },
+    { name: '联系我', href: '#' },
 ]
 
 const mobileMenuOpen = ref(false)
