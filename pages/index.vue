e29fa89cf60dd24f4d70ce4f7daff3e37fbf4464
iwideal <iwideal@outlook.com>
2024-09-25 21:41:15 +0800

    111

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="bg-white">
         <div class="mx-auto w-full px-6 lg:px-8">
-            <div class="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-6">
-                <article v-for="post in posts" :key="post.id"
-                    class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
-                    <img :src="post.imageUrl" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover" />
-                    <div class="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
-                    <div class="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-                    <div class="flex flex-wrap items-center gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">
-                        <time :datetime="post.datetime" class="mr-8">{{ post.date }}</time>
-                        <div class="-ml-4 flex items-center gap-x-4">
-                            <svg viewBox="0 0 2 2" class="-ml-0.5 h-0.5 w-0.5 flex-none fill-white/50">
-                                <circle cx="1" cy="1" r="1" />
-                            </svg>
+            <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+                <article v-for="post in posts" :key="post.id" class="flex flex-col items-start justify-between">
+                    <div class="relative w-full" @click="$router.push(post.href)">
+                        <img :src="post.imageUrl" alt=""
+                            class="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
+                        <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                    </div>
+                    <div class="max-w-xl">
+                        <div class="mt-8 flex items-center gap-x-4 text-xs">
+                            <time class="text-gray-500">{{ post.date }}</time>
+                            <a
+                                class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{
+                                    post.tags[0] }}</a>
+                        </div>
+                        <div class="group relative">
+                            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                <a @click="$router.push(post.href)" :as="button">
+                                    <span class="absolute inset-0" />
+                                    {{ post.title }}
+                                </a>
+                            </h3>
                         </div>
                     </div>
-                    <h3 class="mt-3 text-lg font-semibold leading-6 text-white">
-                        <a :href="post.href">
-                            <span class="absolute inset-0" />
-                            {{ post.title }}
-                        </a>
-                    </h3>
                 </article>
             </div>
         </div>
